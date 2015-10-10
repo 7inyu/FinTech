023493a7d75379de96648e9c9a897011a1498429
7inyu <wang7inyu@gmail.com>
2015-10-10 12:02:40 -0400

    Initial Commit

---
+++ FinTech/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FinTech
+//
+//  Created by Qinyu Wang on 10/10/15.
+//  Copyright (c) 2015 Qinyu Wang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
